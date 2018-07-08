a12bc5e0272be4b4749ab0229c3ce99735b5a066
William Lebel <lebelwilliam@gmail.com>
2018-07-08 00:29:50 -0400

    Add setup.py to allow the use of the site stuff in the bot.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="legendarybot-website",
+    version="0.0.1",
+    author="Greatman",
+    author_email="notyourbusiness@test.com",
+    description="LegendaryBot website",
+    long_description="LegendaryBot website",
+    long_description_content_type="text/markdown",
+    url="https://github.com/LegendaryBot/website",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
